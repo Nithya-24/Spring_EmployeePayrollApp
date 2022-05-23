c02eff88c1a1473c64505be1bd0385b19ff16a42
Your Name <nithyaramadoz@gmail.com>
2022-05-23 10:19:54 +0530

    Added: Storing Employee Payroll Data in list

--- src/main/java/com/example/employeepayrollapp/service/EmployeePayrollServiceImpl.java
+++ src/main/java/com/example/employeepayrollapp/service/EmployeePayrollServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.employeepayrollapp.service;
 
 import com.example.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.example.employeepayrollapp.model.EmployeePayrollData;
-import com.example.employeepayrollapp.repostitory.IEmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,40 +11,37 @@ import java.util.List;
 @Service
 public class EmployeePayrollServiceImpl implements IEmployeePayrollService {
 
-//    @Autowired
-//    private IEmployeeRepository employeeRepository;
+    private List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
 
     @Override
     public List<EmployeePayrollData> getEmployeePayrollData() {
-        List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
-        employeePayrollDataList.add(new EmployeePayrollData(1,new EmployeePayrollDTO("Nithya",4000)));
         return employeePayrollDataList;
     }
 
     @Override
     public EmployeePayrollData getEmployeePayrollDataById(int empId) {
-        EmployeePayrollData employeePayrollData = null;
-        employeePayrollData = new EmployeePayrollData(empId, new EmployeePayrollDTO("Nithya", 6000));
-        return employeePayrollData;
+        return employeePayrollDataList.get(empId);//as arraylist index is starts from 0
     }
 
     @Override
     public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
         EmployeePayrollData employeePayrollData = null;
-        employeePayrollData = new EmployeePayrollData(1, employeePayrollDTO);
+        employeePayrollData = new EmployeePayrollData(employeePayrollDataList.size()+1, employeePayrollDTO);
+        employeePayrollDataList.add(employeePayrollData);//adds in employee list
         return employeePayrollData;
     }
 
     @Override
     public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO) {
-        EmployeePayrollData employeePayrollData = null;
-        employeePayrollData = new EmployeePayrollData(empId, employeePayrollDTO);
+        EmployeePayrollData employeePayrollData = this.getEmployeePayrollDataById(empId);
+        employeePayrollData.setName(employeePayrollDTO.name);
+        employeePayrollData.setSalary(employeePayrollDTO.salary);
+        employeePayrollDataList.set(empId-1,employeePayrollData);
         return employeePayrollData;
     }
 
     @Override
     public void deleteEmployeePayrollData(int empId) {
-        EmployeePayrollData employeePayrollData = null;
-        employeePayrollData = new EmployeePayrollData(empId,new EmployeePayrollDTO("Nithya",8000));
+        employeePayrollDataList.remove(empId-1);
     }
 }
