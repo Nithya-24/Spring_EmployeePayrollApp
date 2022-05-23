fe3feaede56d28116387860e2bb93908f74b419c
Your Name <nithyaramadoz@gmail.com>
2022-05-23 08:26:02 +0530

    Implemented DTO and model

--- src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,5 +1,6 @@
 package com.example.employeepayrollapp;
 
+import ch.qos.logback.core.CoreConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ public class EmployeePayrollAppApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        System.out.println("Welcome");
     }
 
 }
