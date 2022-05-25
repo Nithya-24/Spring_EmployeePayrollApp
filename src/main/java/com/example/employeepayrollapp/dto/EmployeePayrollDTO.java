1fafff4ebb9304de91b866525630cf1bf2594672
Your Name <nithyaramadoz@gmail.com>
2022-05-25 13:19:01 +0530

    Added: Validation done on DTO

--- src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -3,9 +3,7 @@ package com.example.employeepayrollapp.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.beans.factory.annotation.Required;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -18,15 +16,21 @@ public class EmployeePayrollDTO {
     @Min(value = 500, message = "Min wage should be more than 500")
     public long salary;
 
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
     public String gender;
 
     @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "Start date should not be empty")
+    @PastOrPresent(message = "Start date should be past or today's date")
     public LocalDate startDate;
 
+    @NotBlank(message = "Note cannot be empty")
     public String note;
 
+    @NotBlank(message = "Profile pic cannot be empty")
     public String profilePic;
 
+    @NotNull(message = "Department cannot be empty")
     public List<String> department;
 
 }
