a45409e5071d62208c12ee68b69a2b6483fb1a08
Your Name <nithyaramadoz@gmail.com>
2022-05-26 08:33:19 +0530

    Employee Payroll App created using SpringBoot

--- src/main/java/com/example/employeepayrollapp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/example/employeepayrollapp/repository/EmployeePayrollRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData, Integer> {
-    @Query(value = "select * from employee_payroll, employee_department where employee_id = id and department = :department",
+    @Query(value = "select * from employee_payroll, employee_department where emp_id = id and department = :department",
             nativeQuery = true)
     List<EmployeePayrollData> findEmployeeByDepartment(String department);
 }
